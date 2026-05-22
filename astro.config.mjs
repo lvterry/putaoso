cf3f15d4d38e75d7090fca7b50f17f52d068456b
Terry Wang <lvterry@gmail.com>
2026-05-22 12:00:24 +0800

    Improve multilingual SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,22 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://putaoso.com',
   build: {
     format: 'file', // 输出 about.html 而不是 about/index.html
   },
+  integrations: [
+    sitemap({
+      i18n: {
+        defaultLocale: 'zh',
+        locales: {
+          zh: 'zh-CN',
+          en: 'en',
+          de: 'de',
+          es: 'es',
+        },
+      },
+    }),
+  ],
 });
